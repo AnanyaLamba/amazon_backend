24de03ced8ea96d31c177ef6a72e86c938b8129a
Ananya Lamba <ananyalamba2002@gmail.com>
2025-01-22 10:20:52 +0530

    change the cartitems

---
+++ models/cart.js
@@ -0,0 +1,22 @@
+// const moongose = require('mongoose');
+
+// const cartSchema = moongose.Schema({
+//     userId: {
+//         type: String,
+//         required: true,
+//     },
+//     products: [
+//         {
+//         productId: {
+//             type: String,
+//             required: true,
+//         },
+//         quantity: {
+//             type: Number,
+//             required: true,
+//         },
+//         },
+//     ],
+// });
+
+// module.exports = moongose.model('Cart', cartSchema);
